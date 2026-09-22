34e472f2b228c8ad5a10d4c1d73f0c9d8868b0e6
lucassantos-pucc <lucas.os6@puccampinas.edu.br>
2026-09-21 21:46:52 -0300

    criação do servidor

---
+++ BackEnd/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => { //retorno do servidor 
+    res.send("Olá! Meu servidor está funcionando!");
+});
+
+app.listen(3000, () => { //porta em que o server esta sendo rodado
+    console.log("Servidor rodando em http://localhost:3000");
+});
