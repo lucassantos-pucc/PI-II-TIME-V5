1c31502dcd82e235d357a3a312aeb24c8bcf2bbd
VitorKenzo8 <vitor.kpt@puccampinas.edu.br>
2026-09-23 07:29:24 -0300

    Protótipo de função para listagem de demandas dinamicamente por um arquivo json, ainda em desenvolvimento ( 1 hora 30 min )

--- FrontEnd/Scripts/Listagem.js
+++ FrontEnd/Scripts/Listagem.js
@@ -1,3 +1,25 @@
+
+// Feito por: Vitor Kenzo Pina Takemasa
+// RA: 26007167
+
+function CreateDemanda(json) {
+    const info = JSON.parse('{"nome":"Teste", "tipo":"Defeito", "prioridade":"Baixa"}'); // Lista temporaria
+    
+    const Dem_Cr = document.createElement('button'); // Demanda Criada
+
+    Dem_Cr.classList.add("demanda-card");
+
+    const h3 = document.createElement('h3');
+    h3.appendChild(document.createTextNode(info.nome));
+    Dem_Cr.appendChild(h3);
+
+
+    
+    document.getElementById("secao-demanda").appendChild(Dem_Cr);
+    //document.getElementById("secao-demanda").addEventListener("click", ShowDialog(this));
+    // !! Não funciona ainda, testar mais dps
+}
+
 function PriorityColor(p) {
 
     prio = p.textContent.toLowerCase(); // Variável de simplificação da sintaxe
@@ -19,7 +41,8 @@ function PriorityColor(p) {
         p.classList.add("prioridade-c-gen");
     }
 }
-    
+
+
 // Obtém os elementos de prioridade com base na classe geral de prioridade
 const ElementosPrioridade = document.getElementsByClassName("prioridade-gen");
     
