6986a78356e36ae44ce2c52726029da7584c1c2e
arthur1105 <toarthur1105@gmail.com>
2026-09-24 15:22:54 -0300

    Arrumei um bug na listagem

--- FrontEnd/Scripts/Listagem.js
+++ FrontEnd/Scripts/Listagem.js
@@ -2,7 +2,7 @@
 // Feito por: Vitor Kenzo Pina Takemasa
 // RA: 26007167
 
-function CreateDemanda(json) {
+function CreateDemanda(json) { //Função para criar as demandas na pagina
     const info = JSON.parse('{"nome":"Teste", "tipo":"Defeito", "prioridade":"Baixa"}'); // Lista temporaria
     
     const Dem_Cr = document.createElement('button'); // Demanda Criada
@@ -13,16 +13,19 @@ function CreateDemanda(json) {
     h3.appendChild(document.createTextNode(info.nome));
     Dem_Cr.appendChild(h3);
 
-
-    
+    Dem_Cr.addEventListener("click", () => ShowDialog(Dem_Cr)); 
     document.getElementById("secao-demanda").appendChild(Dem_Cr);
-    //document.getElementById("secao-demanda").addEventListener("click", ShowDialog(this));
     // !! Não funciona ainda, testar mais dps
+    // * Tudo aquele sofrimento pq tava dando append antes de adicionar o eventListener e adicionando no h3 ao inves do botao
+    // * e tinha que colocar com arrow function :)
 }
 
 function PriorityColor(p) {
+    if (!p) { //So coloquei isso para evitar problema de estar vazio
+        return;
+    }
 
-    prio = p.textContent.toLowerCase(); // Variável de simplificação da sintaxe
+    const prio = p.textContent.toLowerCase(); // Variável de simplificação da sintaxe
     
     // Aplica as cores de fundo respectivas
     if (prio.includes("baixa")) {
@@ -47,6 +50,6 @@ function PriorityColor(p) {
 const ElementosPrioridade = document.getElementsByClassName("prioridade-gen");
     
 // Itera por todos as prioridades
-for (i in ElementosPrioridade) {
-    PriorityColor(ElementosPrioridade[i]);
+for (const prioridade of ElementosPrioridade) {
+    PriorityColor(prioridade);
 }
