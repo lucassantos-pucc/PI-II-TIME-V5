b8afbed9090f9ad5784a6261c1f96b4a6884fd84
VitorKenzo8 <vitor.kpt@puccampinas.edu.br>
2026-09-25 12:35:42 -0300

    Finalização da função CreateDemanda

--- FrontEnd/Scripts/Listagem.js
+++ FrontEnd/Scripts/Listagem.js
@@ -3,21 +3,51 @@
 // RA: 26007167
 
 function CreateDemanda(json) {
-    const info = JSON.parse('{"nome":"Teste", "tipo":"Defeito", "prioridade":"Baixa"}'); // Lista temporaria
+    const info = JSON.parse('{"nome":"Teste", "tipo":"Defeito", "prioridade":"Baixa", "status":"Em andamento", "projeto":"Exemplo", "responsavel":"Sr. Exemplilson", "DTcriacao":"25/09/2026", "prazo":"30/09/2026"}'); // Lista temporaria
     
     const Dem_Cr = document.createElement('button'); // Demanda Criada
-
     Dem_Cr.classList.add("demanda-card");
 
     const h3 = document.createElement('h3');
     h3.appendChild(document.createTextNode(info.nome));
     Dem_Cr.appendChild(h3);
 
+    Dem_Cr.appendChild(document.createElement('hr'));
+
+    const box = document.createElement('div');
+    box.classList.add("caixa");
+
+    box.appendChild(DemandaContent("tipo-gen", "Tipo: " + info.tipo));
+    box.appendChild(DemandaContent("prioridade-gen", "Prioridade: " + info.prioridade));
+    box.appendChild(DemandaContent("status-gen", "Status: " + info.status));
+    box.appendChild(DemandaContent("projeto-gen", "Projeto: " + info.projeto));
+    box.appendChild(DemandaContent("responsavel-gen", "Responsável: " + info.responsavel));
+    box.appendChild(DemandaContent("DTcriacao-gen", "Data de criacao: " + info.DTcriacao));
+    box.appendChild(DemandaContent("prazo-gen", "Prazo: " + info.prazo));
+
+    Dem_Cr.appendChild(box);
 
     
+    Dem_Cr.addEventListener("click", function() {ShowDialog(this);});
+
     document.getElementById("secao-demanda").appendChild(Dem_Cr);
-    //document.getElementById("secao-demanda").addEventListener("click", ShowDialog(this));
-    // !! Não funciona ainda, testar mais dps
+}
+
+function asd(a) {
+    console.log(a)
+}
+
+function DemandaContent(classe, texto) {
+    const content = document.createElement('div');
+    content.classList.add("gen");
+    content.classList.add(classe);
+
+    const p = document.createElement('p');
+    p.appendChild(document.createTextNode(texto));
+
+    content.appendChild(p);
+    
+    return content;
 }
 
 function PriorityColor(p) {
@@ -41,6 +71,8 @@ function PriorityColor(p) {
         p.classList.add("prioridade-c-gen");
     }
 }
+
+CreateDemanda("temp");
 
 
 // Obtém os elementos de prioridade com base na classe geral de prioridade
